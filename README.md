48c31bb7b767f84562332b7b904f2b8b9ff2503c
mohobson <mohobson8@gmail.com>
2021-10-24 18:00:51 -0400

    cleaned up

---
+++ README.md
@@ -0,0 +1,5 @@
+to run the app:
+
+export FLASK_APP=run.py
+
+flask run
--- run.py
+++ run.py
@@ -2,9 +2,4 @@ from flaskr.app import create_app
 
 app = create_app(test_config=None)
 
-# create_app(test_config=None)
-
-# if __name__=="__main__":
-#     app = create_app(test_config=None)
-#     app.run()
 
